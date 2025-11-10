64a5b9395d2fade91a2b2b756d3fe930826bddf4
jullazarovych <juliala125@gmail.com>
2025-11-10 16:54:12 +0200

    added app & basic structure of project

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app(config_name="default"):
+    app = Flask(__name__, template_folder='templates') 
+    
+    with app.app_context():
+        from . import view
+        app.register_blueprint(view.main_bp)
+    
+    return app  
